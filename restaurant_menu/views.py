b76be86302e3d699f9307b2ae9977a1d5489ba26
Daniel <Pech.Daniel@seznam.cz>
2024-12-15 19:35:48 +0100

    creating def_context_data in views.py testing with html index

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
 from django.views import generic
 from .models import Item
 
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
+
+    def get_context_data(self):
+        context = {"meals": ["Pizza", "Pasta"],
+                   "ingredients": ["things"] }
+        return context
 
 
 
