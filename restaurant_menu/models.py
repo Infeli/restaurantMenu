5bc2d678ac46e45244319099d0db3068253791ae
Daniel <Pech.Daniel@seznam.cz>
2024-12-15 19:28:18 +0100

    creating views + urls

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -20,7 +20,7 @@ class Item(models.Model):
     description = models.CharField(max_length=2000)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     meal_type = models.CharField(max_length=2000, choices=MEAL_TYPE)
-    # Many to one relationships - User / Item
+    # Manny to one relationships - User / Item
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     status = models.IntegerField(choices=STATUS, default=0)
     date_created = models.DateTimeField(auto_now_add=True)
